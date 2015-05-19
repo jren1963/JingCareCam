45fed1dc52be5f76c15253e4cf62534c3c615b84
jren1963 <Jing@Jingqings-MacBook-Pro.local>
2015-05-18 22:45:12 -0400

    6 changes

--- JingCareCam/ViewController.swift
+++ JingCareCam/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleLabel : UILabel!
+    @IBOutlet var messageTextView : UITextView!
+    
+    @IBAction func sendTapped(sender : AnyObject) {
+        
+    }
+    @IBAction func cancelTapped(sender : AnyObject) {
+        
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    let careCam = JingCareCamModel(user: "John", title: "Remind John to enter his weight this morning", message:  "John, Don't forget to enter your weight this morning. It would be great if you did it before lunch. Thanks!")
+    
+    func refreshUI() {
+        titleLabel.text = careCam.title
+        messageTextView.text = careCam.message
+        
+    }
 
 }
 
---
+++ JingCareCamModel.swift
@@ -0,0 +1,30 @@
+//
+//  JingCareCamModel.swift
+//  JingCareCam
+//
+//  Created by Jingqing Ren on 5/18/15.
+//  Copyright (c) 2015 Jingqing Ren. All rights reserved.
+//
+
+import Foundation
+
+class JingCareCamModel {
+    
+    var user: String
+    var title: String
+    var message: String
+    
+    init (user: String, title: String, message: String) {
+        self.user = user
+        self.title = title
+        self.message = message
+    }
+    
+    func populateData() {
+        
+        
+        
+    }
+    
+    
+}
